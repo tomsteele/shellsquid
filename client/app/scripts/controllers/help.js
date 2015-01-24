98160e0a28263cf6a422b4a45c07072b375db548
packetassailant <cpatten@packetresearch.com>
2015-01-24 10:19:26 -0500

    Added the Controller/View/Route associated with with the Help content.

---
+++ client/app/scripts/controllers/help.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:HelpCtrl
+ * @description
+ * # HelpCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('HelpCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
