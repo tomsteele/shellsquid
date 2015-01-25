271ccf0d2cc3bc9413c815ad9af3d6a6fe25ba0f
Tom Steele <thomasjsteele@gmail.com>
2015-01-24 17:14:01 -0800

    beautify

--- client/app/scripts/controllers/help.js
+++ client/app/scripts/controllers/help.js
@@ -8,10 +8,10 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('HelpCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('HelpCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
