d6e05a7410a40dcc84ff7ea8216a547239e759a7
Tom Steele <thomasjsteele@gmail.com>
2015-03-29 14:34:41 -0700

    Updates minor version

--- release.sh
+++ release.sh
@@ -6,8 +6,8 @@ cd ..
 
 for OS in "linux" "darwin" "freebsd"; do
     for ARCH in "amd64"; do
-        GOOS=$OS  CGO_ENABLED=0 GOARCH=$ARCH go build
-        FOLDER=shellsquid2.0.2$OS-$ARCH
+        GOOS=$OS  CGO_ENABLED=0 GOARCH=$ARCH godep go build
+        FOLDER=shellsquid2.1.0$OS-$ARCH
         ARCHIVE=$FOLDER.tar.gz
         mkdir -p $FOLDER/client/dist
         cp LICENSE $FOLDER
